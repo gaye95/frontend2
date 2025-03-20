00dc40253044014b3cd2ef0faed2accfb4c68fab
gaye95 <89346921+gaye95@users.noreply.github.com>
2025-03-20 09:44:17 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    allowedHosts: ["frontend2-1-dpgf.onrender.com"], // Ajoute l'hôte autorisé// Permet d'exposer sur 0.0.0.0 automatiquement
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://backend2-1-173j.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
